5b012c71a5784f79d852a3c3ad72dca7cc13f1a7
Sherif Kamal <sherif.kamal.salem@gmail.com>
2021-08-04 00:31:51 +0200

    Init project

---
+++ iOS-CodeTask/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  iOS-CodeTask
+//
+//  Created by sherif kamal on 04/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
